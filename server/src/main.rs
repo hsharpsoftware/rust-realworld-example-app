732a4dbbd15e33af9a3628cca2bd024656359afe
davidpodhola <david.podhola@naseukoly.cz>
2017-04-28 22:40:23 +0200

    basic mongodb lines

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,15 @@
 #[macro_use]
 extern crate nickel;
 
+#[macro_use(bson, doc)]
+extern crate bson;
+extern crate mongodb;
+
 use nickel::Nickel;
+
+use bson::Bson;
+use mongodb::{Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
 
 fn main() {
     let mut server = Nickel::new();
@@ -11,6 +19,11 @@ fn main() {
             "Hello world!"
         }
     });
+
+    let client =
+        Client::connect("localhost", 27017).expect("Failed to initialize standalone client.");
+
+    let coll = client.db("test").collection("movies");
 
     server.listen("127.0.0.1:6767");
 }
