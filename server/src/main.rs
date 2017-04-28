1a35ef5a0651a3581c7170d970d33bdd1af8f1a0
davidpodhola <david.podhola@naseukoly.cz>
2017-04-28 22:03:18 +0200

    nickel added

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate nickel;
+
+use nickel::Nickel;
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
 }
