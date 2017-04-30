5703ea0bd47b3bc86f548ab6919eb5ada2d586fc
davidpodhola <david.podhola@naseukoly.cz>
2017-04-30 15:01:30 +0200

    allow to run on Azure Web App (see e.g. http://rust-nickel-azure-web-app.azurewebsites.net/)

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,46 +11,59 @@ use bson::Bson;
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
+use std::env;
+
 fn main() {
     let mut server = Nickel::new();
 
     server.utilize(router! {
         get "**" => |_req, _res| {
-            "Hello world!"
+            "Hello from the test application written in Rust on Nickel running in Azure Web App!"
         }
     });
 
-    let client =
-        Client::connect("localhost", 27017).expect("Failed to initialize standalone client.");
+    if cfg!(db = "mongodb") {
+        let client =
+            Client::connect("localhost", 27017).expect("Failed to initialize standalone client.");
 
-    let coll = client.db("test").collection("movies");
+        let coll = client.db("test").collection("movies");
 
-    let doc = doc! { "title" => "Jaws",
+        let doc = doc! { "title" => "Jaws",
                       "array" => [ 1, 2, 3 ] };
 
-    // Insert document into 'test.movies' collection
-    coll.insert_one(doc.clone(), None)
-        .ok()
-        .expect("Failed to insert document.");
+        // Insert document into 'test.movies' collection
+        coll.insert_one(doc.clone(), None)
+            .ok()
+            .expect("Failed to insert document.");
 
-    // Find the document and receive a cursor
-    let mut cursor = coll.find(Some(doc.clone()), None)
-        .ok()
-        .expect("Failed to execute find.");
+        // Find the document and receive a cursor
+        let mut cursor = coll.find(Some(doc.clone()), None)
+            .ok()
+            .expect("Failed to execute find.");
 
-    let item = cursor.next();
+        let item = cursor.next();
 
-    // cursor.next() returns an Option<Result<Document>>
-    match item {
-        Some(Ok(doc)) => {
-            match doc.get("title") {
-                Some(&Bson::String(ref title)) => println!("{}", title),
-                _ => panic!("Expected title to be a string!"),
+        // cursor.next() returns an Option<Result<Document>>
+        match item {
+            Some(Ok(doc)) => {
+                match doc.get("title") {
+                    Some(&Bson::String(ref title)) => println!("{}", title),
+                    _ => panic!("Expected title to be a string!"),
+                }
             }
+            Some(Err(_)) => panic!("Failed to get next from server!"),
+            None => panic!("Server returned no results!"),
         }
-        Some(Err(_)) => panic!("Failed to get next from server!"),
-        None => panic!("Server returned no results!"),
     }
 
-    server.listen("127.0.0.1:6767");
+    let port = match env::var("HTTP_PLATFORM_PORT") {
+        Ok(val) => val,
+        Err(e) => "6767".to_string(),
+    };
+
+    let listenOn = format!("127.0.0.1:{}", port);
+
+    println!("Listening on {}", listenOn);
+
+    server.listen(listenOn);
 }
