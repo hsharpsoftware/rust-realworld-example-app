c66d157cb898030f3d991acc4ad78eddc3d03a10
David Podhola <david.podhola@hsharp.software>
2017-06-29 09:10:00 +0200

    build.cmd also launches the server and run the tests

--- build.cmd
+++ build.cmd
@@ -3,4 +3,11 @@ cargo build
 if errorlevel 1 (
   exit /b %errorlevel%
 )
+start /B "" cargo run
+cargo test
+if errorlevel 1 (
+  taskkill /F /IM server.exe
+  exit /b %errorlevel%
+)
+taskkill /F /IM server.exe
 cd ..
